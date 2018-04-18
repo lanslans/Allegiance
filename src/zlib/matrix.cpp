5d160ae76dab6074faba4b911a7089bad4b38821
BigBetaTester <weimerh@hotmail.com>
2018-04-17 20:24:27 -0400

    Fix Allegiance include path.  Fix AGCGuid.lib path.  Return fabs to abs.  We're building client again.  Still have AGC and Server error.

--- src/zlib/matrix.cpp
+++ src/zlib/matrix.cpp
@@ -576,12 +576,12 @@ Matrix& Matrix::SetInverse(const Matrix& mStart)
         // pick the largest row in the rowth column
         //
 
-        float max    = fabs(m.m_m[row][row]);
+        float max    = abs(m.m_m[row][row]);
         int   rowMax = row;
 
         for (int rowIndex = row + 1; rowIndex < size; rowIndex++) {
-            if (fabs(m.m_m[rowIndex][row]) > max) {
-                max = fabs(m.m_m[rowIndex][row]);
+            if (abs(m.m_m[rowIndex][row]) > max) {
+                max = abs(m.m_m[rowIndex][row]);
                 rowMax = rowIndex;
             }
         }
